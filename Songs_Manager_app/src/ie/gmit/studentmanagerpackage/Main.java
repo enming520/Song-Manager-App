8c140a279890134d5617eef21ed161b74f99a59b
Enming Li <38397171+enming520@users.noreply.github.com>
2023-03-30 18:02:31 +0100

    Add files via upload

---
+++ Songs_Manager_app/src/ie/gmit/studentmanagerpackage/Main.java
@@ -0,0 +1,18 @@
+package ie.gmit.studentmanagerpackage;
+
+/**
+ * The Main class holds the main method.
+ */
+public class Main {
+	
+	// Main method containing print statement.
+	public static void main(String[] args) {
+		
+		// Create menu object
+		Menu menuObject = new Menu();
+		// Run menu start method
+		menuObject.start();
+		
+	} // End main method
+	
+} // End Main Class
